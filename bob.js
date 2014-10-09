4a1a1e7ad5b528ae1f37cbc53d536ba400813a7f
John Yeglinski <jmyeglinski@gmail.com>
2014-10-09 08:48:36 -0400

    New changelist

---
+++ bob.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+//
+// YOUR CODE GOES HERE
+//
+    var a = 1;
+
+};
+
+module.exports = Bob;
