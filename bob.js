75c72835c5cb5929b7e5ce9d881e545ef7bbdd8b
John Yeglinski <jmyeglinski@gmail.com>
2014-10-10 15:08:03 -0400

    first exercism problem.

--- bob.js
+++ bob.js
@@ -6,11 +6,32 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
-    var a = 1;
+    // creates the endsWith method on the string prototype.
+    if (typeof String.prototype.endsWith != 'function') {
+        // see below for better implementation!
+        String.prototype.endsWith = function (str){
+            return this.indexOf(str) == this.length-1;
+        };
+    }
+    // removes all chars that are not letters
+    var scrubbedInput = input.replace(/[^äöüÄÖÜßa-zA-Z]/g, '');
+    // removes all chars that are not capital letters
+    var onlyCapsInput = scrubbedInput.replace(/[^ÄÖÜßA-Z]/g, '');
+    // regex to remove all chars.
+    var onlySpacesInput = input.replace(/[^ ]/g,'');
+    // base response
+    var response = "Whatever.";
 
+    if(input.endsWith("?"))
+        response = "Sure.";
+    // Checks if all letters are caps
+    if ((onlyCapsInput.length !== 0) && onlyCapsInput.length === scrubbedInput.length)
+        response = "Whoa, chill out!";
+    // checks if input is empty string.
+    if(input.length === 0 || onlySpacesInput .length === input.length)
+        response = 'Fine. Be that way!';
+
+   return response;
 };
 
 module.exports = Bob;
